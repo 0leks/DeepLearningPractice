8052e2d5cb93ab919b7344390733d2d0e1c66f7f
Alex Korshak <alexokorshak@gmail.com>
2019-02-04 17:24:53 -0700

    Add QOL improvements

--- MNIST_VAE_noeager/Main.py
+++ MNIST_VAE_noeager/Main.py
@@ -1,56 +1,77 @@
-# from https://github.com/shaohua0116/VAE-Tensorflow
+# adapted from https://github.com/shaohua0116/VAE-Tensorflow
 
+import vae_util
 
 import numpy as np
-import tensorflow as tf
 import matplotlib
-matplotlib.use('Agg')
+matplotlib.use('Agg') # Allows generating plots without popup. Must call before importing pyplot.
 import matplotlib.pyplot as plt
-import vae_util
-from vae_util import Test
-from vae_util import VariantionalAutoencoder
 
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 
-test = Test()
-print(test.a)
-print(test.b)
-
+# turns off interactive mode
 plt.ioff()
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True, validation_size=10000)
 num_sample = mnist.train.num_examples
+print('train shape: ', mnist.train.images.shape)
+print('test shape: ', mnist.test.images.shape)
+print('validate shape: ', mnist.validation.images.shape)
 input_dim = mnist.train.images[0].shape[0]
 w = h = 28
 
-BATCH_SIZE = 100
+BATCH_SIZE = 200
+NUM_EPOCHS = 10
+LATENT_DIM = 2
 
-partOneEpochs = 0
-partTwoEpochs = 50
+# resets tensorflow graph, use if training multiple graphs in same session
+#tf.reset_default_graph()
 
-if(partOneEpochs):
-  print('Training model with latent dimensions=5')
-  # Train the model
-  model = vae_util.trainer(input_dim, num_sample, mnist, learning_rate=1e-4,  batch_size=BATCH_SIZE, num_epoch=partOneEpochs, n_z=5)
+if(NUM_EPOCHS):
+  print('Training model with', LATENT_DIM, 'latent dimensions and', NUM_EPOCHS, 'epochs')
+  # Train the model with 2d latent space
+  model, trainLoss, valLoss, testLoss = vae_util.trainer(input_dim, num_sample, mnist, learning_rate=1e-4, batch_size=BATCH_SIZE, num_epoch=NUM_EPOCHS, n_z=LATENT_DIM)
 
-  # Test the trained model: reconstruction
-  batch = mnist.test.next_batch(100)
-  x_reconstructed = model.reconstructor(batch[0])
+  # plot losses
+  fig, ax = plt.subplots()
+  t = np.arange(0, NUM_EPOCHS, 1)
+  ax.plot(t, trainLoss, label='Training Set')
+  ax.plot(t, valLoss, label='Validation Set')
+  ax.plot(t, testLoss, label='Test Set')
+  ax.set(xlabel='epoch', ylabel='loss', title='Losses')
+  legend = ax.legend()
+  ax.grid()
+  fig.savefig("losses.png")
 
-  n = np.sqrt(model.batch_size).astype(np.int32)
-  I_reconstructed = np.empty((h*n, 2*w*n))
-  for i in range(n):
-    for j in range(n):
-      x = np.concatenate(
-        (x_reconstructed[i*n+j, :].reshape(h, w),
-         batch[0][i*n+j, :].reshape(h, w)),
-        axis=1
-      )
-      I_reconstructed[i*h:(i+1)*h, j*2*w:(j+1)*2*w] = x
+  # Test the trained model: transformation
+  batch = mnist.test.next_batch(3000)[0]
+  z = model.transformer(batch)
+  fig, ax = plt.subplots()
+  plt.scatter(z[:, 0], z[:, 1], c=np.argmax(batch[1], 1), alpha=0.5)
+  ax.set_xlabel(r'$\mu$')
+  ax.set_ylabel(r'$\sigma$')
+  ax.set_title(r'Latent $\mu$ and $\sigma$ for 3000 random test images')
+  plt.colorbar()
+  plt.grid()
+  plt.savefig('I_transformed.png')
+  plt.close(fig)
+
+  # Test the trained model: continuous latent space
+  n = 20
+  x = np.linspace(-2, 2, n)
+  y = np.linspace(-2, 2, n)
+
+  I_latent = np.empty((h*n, w*n))
+  for i, yi in enumerate(x):
+    for j, xi in enumerate(y):
+      z = np.array([[xi, yi]] * model.batch_size)
+      x_hat = model.generator(z)
+      I_latent[(n-i-1)*28:(n-i)*28, j*28:(j+1)*28] = x_hat[0].reshape(28, 28)
 
   fig = plt.figure()
-  plt.imshow(I_reconstructed, cmap='gray')
-  plt.savefig('I_reconstructed.png')
+  plt.imshow(I_latent, cmap="gray")
+  plt.savefig('I_latent.png')
   plt.close(fig)
 
   # Test the trained model: generation
@@ -69,36 +90,21 @@ if(partOneEpochs):
   plt.savefig('I_generated.png')
   plt.close(fig)
 
-tf.reset_default_graph()
-
-if(partTwoEpochs):
-  print('Training model with latent dimensions=2')
-  # Train the model with 2d latent space
-  model_2d = vae_util.trainer(input_dim, num_sample, mnist, learning_rate=1e-4,  batch_size=100, num_epoch=partTwoEpochs, n_z=2)
-
-  # Test the trained model: transformation
-  batch = mnist.test.next_batch(3000)
-  z = model_2d.transformer(batch[0])
-  fig = plt.figure()
-  plt.scatter(z[:, 0], z[:, 1], c=np.argmax(batch[1], 1))
-  plt.colorbar()
-  plt.grid()
-  plt.savefig('I_transformed.png')
-  plt.close(fig)
-
-  # Test the trained model: transformation
-  n = 20
-  x = np.linspace(-2, 2, n)
-  y = np.linspace(-2, 2, n)
-
-  I_latent = np.empty((h*n, w*n))
-  for i, yi in enumerate(x):
-    for j, xi in enumerate(y):
-      z = np.array([[xi, yi]]*model_2d.batch_size)
-      x_hat = model_2d.generator(z)
-      I_latent[(n-i-1)*28:(n-i)*28, j*28:(j+1)*28] = x_hat[0].reshape(28, 28)
+  # Test the trained model: reconstruction
+  n = 10
+  batch = mnist.test.next_batch(n*n)[0]
+  x_reconstructed = model.reconstructor(batch)
+  I_reconstructed = np.empty((h*n, 2*w*n))
+  for i in range(n):
+    for j in range(n):
+      x = np.concatenate(
+        (x_reconstructed[i*n+j, :].reshape(h, w),
+         batch[0][i*n+j, :].reshape(h, w)),
+        axis=1
+      )
+      I_reconstructed[i*h:(i+1)*h, j*2*w:(j+1)*2*w] = x
 
   fig = plt.figure()
-  plt.imshow(I_latent, cmap="gray")
-  plt.savefig('I_latent.png')
+  plt.imshow(I_reconstructed, cmap='gray')
+  plt.savefig('I_reconstructed.png')
   plt.close(fig)
--- MNIST_VAE_noeager/vae_util.py
+++ MNIST_VAE_noeager/vae_util.py
@@ -2,7 +2,6 @@
 import numpy as np
 import tensorflow as tf
 import time
-from tensorflow.contrib.slim import fully_connected as fc
 from tensorflow.contrib import slim
 
 
@@ -11,8 +10,12 @@ def trainer(input_dim, num_samples, dataset, learning_rate=1e-3, batch_size=100,
   model = VariantionalAutoencoder(input_dim, learning_rate=learning_rate,
                                   batch_size=batch_size, n_z=n_z)
 
+  totalLoss = []
+  testLoss = []
+  validateLoss = []
   for epoch in range(num_epoch):
     start_time = time.time()
+    epochLoss = []
     for iter in range(num_samples // batch_size):
       # Obtain a batch
       batch = dataset.train.next_batch(batch_size)
@@ -21,12 +24,27 @@ def trainer(input_dim, num_samples, dataset, learning_rate=1e-3, batch_size=100,
       #print(batch.shape)
       # Execute the forward and the backward pass and report computed losses
       loss, recon_loss, latent_loss = model.run_single_step(batch)
+      epochLoss.append(loss)
+    totalLoss.append(sum(epochLoss) / len(epochLoss))
     delta_time = time.time() - start_time
+
+    batch = dataset.validation.next_batch(10000)[0]
+    x_hat = model.reconstructor(batch)
+    vloss, _, _ = model.compute_loss(batch)
+    validateLoss.append(vloss)
+
+    batch = dataset.test.next_batch(10000)[0]
+    x_hat = model.reconstructor(batch)
+    tloss, _, _ = model.compute_loss(batch)
+    testLoss.append(tloss)
+
     if epoch % 1 == 0:
       print('[Epoch {} Time {}s] Loss: {}, Recon loss: {}, Latent loss: {}'.format(epoch, delta_time, loss, recon_loss, latent_loss))
-
+  print('training losses: ', totalLoss)
+  print('validation losses: ', validateLoss)
+  print('testing losses: ', testLoss)
   print('Done!')
-  return model
+  return model, totalLoss, validateLoss, testLoss
 
 
 class VariantionalAutoencoder(object):
@@ -39,6 +57,7 @@ class VariantionalAutoencoder(object):
 
     self.build()
 
+    #self.sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=True))
     self.sess = tf.InteractiveSession()
     self.sess.run(tf.global_variables_initializer())
 
@@ -48,31 +67,31 @@ class VariantionalAutoencoder(object):
 
     # Encode
     # x -> z_mean, z_sigma -> z
-    net = tf.reshape(self.x, [-1, 28, 28, 1], name='reshape1')
-    net = slim.conv2d(net, 32, [3, 3], scope='conv1_1', activation_fn=tf.nn.elu)
-    net = slim.max_pool2d(net, [2, 2], scope='pool1')
+    # net = tf.reshape(self.x, [-1, 28, 28, 1], name='reshape1')
+    # net = slim.conv2d(net, 32, [3, 3], scope='conv1_1', activation_fn=tf.nn.elu)
+    # net = slim.max_pool2d(net, [2, 2], scope='pool1')
     #net = slim.conv2d(net, 128, [3, 3], scope='conv3_2')
     #net = slim.conv2d(net, 128, [3, 3], scope='conv3_3')
     #net = slim.max_pool2d(net, [2, 2], scope='pool2')
-    net = slim.flatten(net)
-    #f1 = fc(net, 512, scope='enc_fc1', activation_fn=tf.nn.elu)
-    #f2 = fc(f1, 384, scope='enc_fc2', activation_fn=tf.nn.elu)
-    f3 = fc(net, 256, scope='enc_fc3', activation_fn=tf.nn.elu)
-    self.z_mu = fc(f3, self.n_z, scope='enc_fc4_mu', activation_fn=None)
-    self.z_log_sigma_sq = fc(f3, self.n_z, scope='enc_fc4_sigma', activation_fn=None)
+    #net = slim.flatten(net)
+    net = slim.fully_connected(self.x, 512, scope='enc_fc1', activation_fn=tf.nn.elu)
+    net = slim.fully_connected(net, 384, scope='enc_fc2', activation_fn=tf.nn.elu)
+    f3 = slim.fully_connected(net, 256, scope='enc_fc3', activation_fn=tf.nn.elu)
+    self.z_mu = slim.fully_connected(f3, self.n_z, scope='enc_fc4_mu', activation_fn=None)
+    self.z_log_sigma_sq = slim.fully_connected(f3, self.n_z, scope='enc_fc4_sigma', activation_fn=None)
     eps = tf.random_normal(shape=tf.shape(self.z_log_sigma_sq),
                            mean=0, stddev=1, dtype=tf.float32)
     self.z = self.z_mu + tf.sqrt(tf.exp(self.z_log_sigma_sq)) * eps
 
     # Decode
     # z -> x_hat
-    g1 = fc(self.z, 256, scope='dec_fc1', activation_fn=tf.nn.elu)
-    net = tf.reshape(g1, [-1, 16, 16, 1], name='reshape1')
-    net = slim.conv2d_transpose(net, 8, 3, activation_fn=tf.nn.elu)
-    net = slim.flatten(net)
-    #g2 = fc(g1, 384, scope='dec_fc2', activation_fn=tf.nn.elu)
-    #g3 = fc(net, 512, scope='dec_fc3', activation_fn=tf.nn.elu)
-    self.x_hat = fc(net, 784, scope='dec_fc4', activation_fn=tf.sigmoid)
+    net = slim.fully_connected(self.z, 256, scope='dec_fc1', activation_fn=tf.nn.elu)
+    # net = tf.reshape(g1, [-1, 16, 16, 1], name='reshape1')
+    # net = slim.conv2d_transpose(net, 8, 3, activation_fn=tf.nn.elu)
+    # net = slim.flatten(net)
+    net = slim.fully_connected(net, 384, scope='dec_fc2', activation_fn=tf.nn.elu)
+    net = slim.fully_connected(net, 512, scope='dec_fc3', activation_fn=tf.nn.elu)
+    self.x_hat = slim.fully_connected(net, 784, scope='dec_fc4', activation_fn=tf.sigmoid)
 
     # Loss
     # Reconstruction loss
@@ -105,6 +124,14 @@ class VariantionalAutoencoder(object):
     )
     return loss, recon_loss, latent_loss
 
+  def compute_loss(self, x):
+    loss, recon_loss, latent_loss = self.sess.run(
+      [self.total_loss, self.recon_loss, self.latent_loss],
+      feed_dict={self.x: x}
+    )
+    return loss, recon_loss, latent_loss
+
+
   # x -> x_hat
   def reconstructor(self, x):
     x_hat = self.sess.run(self.x_hat, feed_dict={self.x: x})
@@ -119,10 +146,3 @@ class VariantionalAutoencoder(object):
   def transformer(self, x):
     z = self.sess.run(self.z, feed_dict={self.x: x})
     return z
-
-
-class Test(object):
-  def __init__(self):
-    self.a = 10
-    self.b = 20
-
