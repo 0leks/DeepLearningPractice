b1b79685f7a7d74146ccae7ed0742c4ef6af399c
Alex Korshak <alexokorshak@gmail.com>
2019-02-11 17:39:36 -0700

    Clean up plots

---
+++ MNIST_VAE_noeager_conv/Main.py
@@ -0,0 +1,162 @@
+# adapted from https://github.com/shaohua0116/VAE-Tensorflow
+
+import vae_util
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg') # Allows generating plots without popup. Must call before importing pyplot.
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+# turns off interactive mode
+plt.ioff()
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True, validation_size=10000)
+num_sample = mnist.train.num_examples
+print('train shape: ', mnist.train.images.shape)
+print('test shape: ', mnist.test.images.shape)
+print('validate shape: ', mnist.validation.images.shape)
+input_dim = mnist.train.images[0].shape[0]
+w = h = 28
+
+BATCH_SIZE = 100
+NUM_EPOCHS = 50
+LATENT_DIM = 2
+
+# resets tensorflow graph, use if training multiple graphs in same session
+#tf.reset_default_graph()
+
+print('Training model with', LATENT_DIM, 'latent dimensions and', NUM_EPOCHS, 'epochs')
+# Train the model with 2d latent space
+model, trainLoss, valLoss, testLoss = vae_util.trainer(input_dim, num_sample, mnist, learning_rate=2e-4, batch_size=BATCH_SIZE, num_epoch=NUM_EPOCHS, n_z=LATENT_DIM)
+
+#with plt.xkcd():
+print('Best validation loss:', min(valLoss))
+###### plot losses ######
+fig, ax = plt.subplots()
+t = np.arange(0, NUM_EPOCHS, 1)
+ax.plot(t, trainLoss, label='Training Set (50000)')
+ax.plot(t, valLoss, label='Validation Set (10000)')
+# don't plot test set losses as it might influence thoughts to see results on test set.
+#ax.plot(t, testLoss, label='Test Set (10000)')
+ax.set(xlabel='epoch', ylabel='loss', title='Losses avg(sum(crossentropy of 784 pixels))')
+ax.legend()
+ax.grid()
+fig.savefig("losses.png")
+
+###### Test the trained model: transformation by sampling from Normal(mu, sigma) for each latent dimension ######
+# I decided this isnt meaningful
+batch = mnist.validation.next_batch(10000)
+# z = model.transformerSample(batch[0])
+# fig, ax = plt.subplots()
+# #averageMu = z[:, 2]+z[:,3]
+# ax.scatter(z[:, 0], z[:, 1], c=np.argmax(batch[1], 1), alpha=0.3)
+# ax.set_xlabel(r'N($\mu_0$,$\sigma_0$)')
+# ax.set_ylabel(r'N($\mu_1$,$\sigma_1$)')
+# ax.set_title(r'Latent N($\mu_0$,$\sigma_0$) and N($\mu_1$,$\sigma_1$) for 10000 validation images')
+# #fig.colorbar()
+# ax.grid()
+# fig.savefig('I_transformed.png')
+
+
+###### Test the trained model: transformation plot only mu ######
+#batch = mnist.test.next_batch(3000)
+z = model.transformer2(batch[0])
+fig, ax = plt.subplots()
+colorValues = np.argmax(batch[1], 1)# * 9 / 8 - 0.5
+s = ax.scatter(z[:, 0], z[:, 1], s=4, c=colorValues, alpha=0.3, cmap='tab10')
+#s = ax.scatter(z[:, 0], z[:, 1], c=np.argmax(batch[1], 1), alpha=0.3)
+ax.set_xlabel(r'$\mu_0$')
+ax.set_ylabel(r'$\mu_1$')
+ax.set_title(r'Latent $\mu_0$ and $\mu_1$ for 10000 validation images')
+cbar = fig.colorbar(s, ax=ax)
+ax.grid()
+fig.savefig('transformed.png')
+
+###### Test the trained model: continuous latent space ######
+n = 80
+minRange = -4
+maxRange = 4
+x = np.linspace(minRange, maxRange, n)
+y = np.linspace(minRange, maxRange, n)
+
+I_latent = np.empty((h*n, w*n))
+num_extra_latent = LATENT_DIM - 2
+extra_dim = []
+if num_extra_latent > 0:
+  extra_dim = [0] * num_extra_latent
+for i, yi in enumerate(x):
+  for j, xi in enumerate(y):
+    #dim = [xi, yi] * int(LATENT_DIM/2)
+    dim = np.concatenate(([xi, yi], extra_dim))
+    z = np.array([dim] * model.batch_size)
+    x_hat = model.generator(z)
+    I_latent[(n-i-1)*28:(n-i)*28, j*28:(j+1)*28] = x_hat[0].reshape(28, 28)
+
+fig = plt.figure(num=None, figsize=(6.4, 4.8), dpi=int(n*28/2))
+
+plt.imshow(I_latent, cmap="gray")
+plt.xticks((0, n*28), (minRange, maxRange))
+plt.yticks((0, n*28), (minRange, maxRange))
+plt.xlabel(r'$\mu_0$')
+plt.ylabel(r'$\mu_1$')
+plt.title('Sampling latent space across first two variables (others set to 0)')
+plt.savefig('latent.png')
+plt.close(fig)
+
+
+fig = plt.figure(num=None, figsize=(6.4, 4.8), dpi=int(n*28/2))
+plt.imshow(-I_latent, cmap="gray")
+plt.xticks((0, n*28), (minRange, maxRange))
+plt.yticks((0, n*28), (minRange, maxRange))
+plt.xlabel(r'$\mu_0$')
+plt.ylabel(r'$\mu_1$')
+plt.title('Sampling latent space across first two variables (others set to 0)')
+plt.savefig('latent_inverted.png')
+plt.close(fig)
+
+###### Test the trained model: generation ######
+# Sample noise vectors from N(0, 1)
+z = np.random.normal(size=[model.batch_size, model.n_z])
+x_generated = model.generator(z)
+
+n = np.sqrt(model.batch_size).astype(np.int32)
+I_generated = np.empty((h*n, w*n))
+for i in range(n):
+  for j in range(n):
+    I_generated[i*h:(i+1)*h, j*w:(j+1)*w] = x_generated[i*n+j, :].reshape(28, 28)
+
+fig = plt.figure()
+plt.imshow(I_generated, cmap='gray')
+plt.xticks([]);
+plt.yticks([]);
+plt.title(r'Generate images by sampling all $\mu$ and $\sigma$ from N(0,1) as input')
+plt.savefig('generated.png')
+plt.close(fig)
+
+# Test the trained model: reconstruction
+n = 10
+batch = mnist.validation.next_batch(n*n)
+x_reconstructed = model.reconstructor(batch[0])
+I_reconstructed = np.empty((h*n, 2*w*n))
+for i in range(n):
+  for j in range(n):
+    x = np.concatenate(
+      (
+        batch[0][i*n+j, :].reshape(h, w),
+        x_reconstructed[i*n+j, :].reshape(h, w)
+      ) , axis=1
+    )
+    I_reconstructed[i*h:(i+1)*h, j*2*w:(j+1)*2*w] = x
+
+fig = plt.figure()
+plt.imshow(I_reconstructed, cmap='gray')
+plt.yticks([]);
+plt.xticks((np.arange(n*2) + 0.5)*28, ('orig', 'recon')*n, rotation=-90);
+plt.title('Original vs Reconstructed validation images')
+plt.savefig('reconstructed.png')
+plt.close(fig)
+
+print('finished')
---
+++ MNIST_VAE_noeager_conv/vae_util.py
@@ -0,0 +1,164 @@
+
+import numpy as np
+import tensorflow as tf
+import time
+from tensorflow.contrib import slim
+
+
+def trainer(input_dim, num_samples, dataset, learning_rate=1e-3, batch_size=100, num_epoch=75, n_z=10):
+  #input_dim = (28,28)
+  model = VariantionalAutoencoder(input_dim, learning_rate=learning_rate,
+                                  batch_size=batch_size, n_z=n_z)
+
+  totalLoss = []
+  testLoss = []
+  validateLoss = []
+  for epoch in range(num_epoch):
+    start_time = time.time()
+    epochLoss = []
+    for iter in range(num_samples // batch_size):
+      # Obtain a batch
+      batch = dataset.train.next_batch(batch_size)
+      #batch = batch[0].reshape([batch_size, 28, 28, 1])
+      batch = batch[0]
+      #print(batch.shape)
+      # Execute the forward and the backward pass and report computed losses
+      loss, recon_loss, latent_loss = model.run_single_step(batch)
+      epochLoss.append(loss)
+    totalLoss.append(sum(epochLoss) / len(epochLoss))
+    delta_time = time.time() - start_time
+
+    batch = dataset.validation.next_batch(10000)[0]
+    x_hat = model.reconstructor(batch)
+    vloss, _, _ = model.compute_loss(batch)
+    validateLoss.append(vloss)
+
+    batch = dataset.test.next_batch(10000)[0]
+    x_hat = model.reconstructor(batch)
+    tloss, _, _ = model.compute_loss(batch)
+    testLoss.append(tloss)
+
+    if epoch % 1 == 0:
+      print('[Epoch {} Time {}s] Loss: {}, Recon loss: {}, Latent loss: {}'.format(epoch, delta_time, loss, recon_loss, latent_loss))
+  print('training losses: ', totalLoss)
+  print('validation losses: ', validateLoss)
+  print('testing losses: ', testLoss)
+  saver = tf.train.Saver()
+  save_path = saver.save(model.sess, "tmp/model.ckpt")
+  print('Model saved in path:', save_path)
+  print('Done!')
+  return model, totalLoss, validateLoss, testLoss
+
+
+class VariantionalAutoencoder(object):
+
+  def __init__(self, input_dim, learning_rate=1e-3, batch_size=100, n_z=10):
+    self.learning_rate = learning_rate
+    self.batch_size = batch_size
+    self.n_z = n_z
+    self.input_dim = input_dim
+
+    self.build()
+
+    #self.sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=True))
+    self.sess = tf.InteractiveSession()
+    self.sess.run(tf.global_variables_initializer())
+    self.writer = tf.summary.FileWriter('graphs', self.sess.graph)
+
+  # Build the netowrk and the loss functions
+  def build(self):
+    self.x = tf.placeholder(name='x', dtype=tf.float32, shape=[None, 784])
+
+    # Encode
+    # x -> z_mean, z_sigma -> z
+    # net = tf.reshape(self.x, [-1, 28, 28, 1], name='reshape1')
+    # net = slim.conv2d(net, 32, [3, 3], scope='conv1_1', activation_fn=tf.nn.elu)
+    # net = slim.max_pool2d(net, [2, 2], scope='pool1')
+    # net = slim.conv2d(net, 32, [3, 3], scope='conv3_2')
+    # net = slim.max_pool2d(net, [2, 2], scope='pool2')
+    # #net = slim.conv2d(net, 128, [3, 3], scope='conv3_3')
+    # #net = slim.max_pool2d(net, [2, 2], scope='pool3')
+    # net = slim.flatten(net)
+    net = slim.fully_connected(self.x, 600, scope='enc_fc1', activation_fn=tf.nn.elu)
+    net = slim.dropout(net, 0.75, scope='dropout1')
+    net = slim.fully_connected(net, 400, scope='enc_fc2', activation_fn=tf.nn.elu)
+    net = slim.dropout(net, 0.75, scope='dropout2')
+    f3 = slim.fully_connected(net, 200, scope='enc_fc3', activation_fn=tf.nn.elu)
+    net = slim.dropout(net, 0.75, scope='dropout6')
+    self.z_mu = slim.fully_connected(f3, self.n_z, scope='enc_fc4_mu', activation_fn=None)
+    self.z_log_sigma_sq = slim.fully_connected(f3, self.n_z, scope='enc_fc4_sigma', activation_fn=None)
+    eps = tf.random_normal(shape=tf.shape(self.z_log_sigma_sq),
+                           mean=0, stddev=1, dtype=tf.float32)
+    self.z = self.z_mu + tf.sqrt(tf.exp(self.z_log_sigma_sq)) * eps
+
+    # Decode
+    # z -> x_hat
+    net = slim.fully_connected(self.z, 200, scope='dec_fc1', activation_fn=tf.nn.elu)
+    net = slim.dropout(net, 0.75, scope='dropout3')
+    # net = tf.reshape(g1, [-1, 16, 16, 1], name='reshape1')
+    # net = slim.conv2d_transpose(net, 8, 3, activation_fn=tf.nn.elu)
+    # net = slim.flatten(net)
+    net = slim.fully_connected(net, 400, scope='dec_fc2', activation_fn=tf.nn.elu)
+    net = slim.dropout(net, 0.75, scope='dropout4')
+    net = slim.fully_connected(net, 600, scope='dec_fc3', activation_fn=tf.nn.elu)
+    net = slim.dropout(net, 0.75, scope='dropout5')
+    self.x_hat = slim.fully_connected(net, 784, scope='dec_fc4', activation_fn=tf.sigmoid)
+
+    # Loss
+    # Reconstruction loss
+    # Minimize the cross-entropy loss
+    # H(x, x_hat) = -\Sigma x*log(x_hat) + (1-x)*log(1-x_hat)
+    epsilon = 1e-10
+    recon_loss = -tf.reduce_sum(
+      self.x * tf.log( epsilon +self.x_hat) + ( 1 -self.x) * tf.log( epsilon + 1 -self.x_hat),
+      axis=1
+    )
+    self.recon_loss = tf.reduce_mean(recon_loss)
+
+    # Latent loss
+    # Kullback Leibler divergence: measure the difference between two distributions
+    # Here we measure the divergence between the latent distribution and N(0, 1)
+    latent_loss = -0.5 * tf.reduce_sum(
+      1 + self.z_log_sigma_sq - tf.square(self.z_mu) - tf.exp(self.z_log_sigma_sq), axis=1)
+    self.latent_loss = tf.reduce_mean(latent_loss)
+
+    self.total_loss = tf.reduce_mean(recon_loss + latent_loss)
+    self.train_op = tf.train.AdamOptimizer(
+      learning_rate=self.learning_rate).minimize(self.total_loss)
+    return
+
+  # Execute the forward and the backward pass
+  def run_single_step(self, x):
+    _, loss, recon_loss, latent_loss = self.sess.run(
+      [self.train_op, self.total_loss, self.recon_loss, self.latent_loss],
+      feed_dict={self.x: x}
+    )
+    return loss, recon_loss, latent_loss
+
+  def compute_loss(self, x):
+    loss, recon_loss, latent_loss = self.sess.run(
+      [self.total_loss, self.recon_loss, self.latent_loss],
+      feed_dict={self.x: x}
+    )
+    return loss, recon_loss, latent_loss
+
+
+  # x -> x_hat
+  def reconstructor(self, x):
+    x_hat = self.sess.run(self.x_hat, feed_dict={self.x: x})
+    return x_hat
+
+  # z -> x
+  def generator(self, z):
+    x_hat = self.sess.run(self.x_hat, feed_dict={self.z: z})
+    return x_hat
+
+  # x -> z
+  def transformerSample(self, x):
+    z = self.sess.run(self.z, feed_dict={self.x: x})
+    return z
+
+  # x -> z_mu
+  def transformer2(self, x):
+    z_mu = self.sess.run(self.z_mu, feed_dict={self.x: x})
+    return z_mu
