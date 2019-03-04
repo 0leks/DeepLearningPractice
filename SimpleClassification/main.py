3195f4ed14e2cd297f60f58d4686c0f2782ec0e1
Alex Korshak <alexokorshak@gmail.com>
2019-03-04 15:30:57 -0700

    made shared venvs tf1 and tf2

---
+++ SimpleClassification/main.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+
+import matplotlib
+matplotlib.use('Agg') # Allows generating plots without popup. Must call before importing pyplot.
+import matplotlib.pyplot as plt
+
+import utils.general
+import utils.data
+import utils.models
+
+# turns off interactive mode for pyplot
+plt.ioff()
+
+print('tensorflow version:', tf.__version__)
+
+(xtrain, ytrain), (xvalidate, yvalidate) = utils.data.getMNISTdatasetClassification(validation_ratio=1/6)
+
+class_names = ['T-shirt', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+modelShape = [512, 256, 128, 10]
+model = utils.models.makeDenseClassifierModel((28, 28, 1), [512, 256, 128, 10])
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+numEpochs = 50
+trainLoss = [0] * numEpochs
+validationLoss = [0] * numEpochs
+
+for i in range(numEpochs):
+    # no batches here, just doing entire dataset
+    model.fit(xtrain, ytrain, epochs=1)
+
+    train_loss, train_acc = model.evaluate(xtrain, ytrain)
+    trainLoss[i] = train_loss
+
+    validation_loss, validation_acc = model.evaluate(xvalidate, yvalidate)
+    validationLoss[i] = validation_loss
+
+# TODO add title and save figure as png instead of displaying plot
+# TODO create local library for saving images, loading data
+plt.figure()
+plt.plot(trainLoss, label='Train Set')
+plt.plot(validationLoss, label='Validation Set')
+plt.xlabel('Epoch')
+plt.ylabel('Loss (sparse_categorical_crossentropy')
+plt.legend()
+plt.title('Dense classifier(' + str(modelShape) + ')')
+plt.savefig('losses.png')
